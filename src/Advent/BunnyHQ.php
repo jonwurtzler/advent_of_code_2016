dcec48d8acd49e296bd55b27748ab847c2fb5761
Jon Wurtzler <jwurtzle@jwurtzle-osx.local>
2017-01-05 18:15:38 -0600

    [Day 1] - Step 2 - Stop at first duplicate point

--- src/Advent/BunnyHQ.php
+++ src/Advent/BunnyHQ.php
@@ -4,8 +4,14 @@ namespace Advent;
 
 use DataLoader;
 
-// Step1: 292 is too high
-// Step2: 288 is the right answer
+// Step 1: 292 is too high
+// Step 1: 288 is the right answer
+
+// Step 2: 275 is too high
+// Step 2: 22 is not right.
+// Step 2: 273 is not right.
+// Step 2: 9 is not right.
+// Step 2: 111 is the correct answer
 class BunnyHQ implements AdventOutputInterface
 {
     /**
@@ -22,9 +28,11 @@ class BunnyHQ implements AdventOutputInterface
     {
         $directions = DataLoader::loadFileAsArrayData($this->dataFile);
 
-        $blocksAway = $this->followDirections($directions);
+        $blocksAway1 = $this->followDirections($directions);
+        $blocksAway2 = $this->followDirections($directions, true);
 
-        echo (sprintf('Santa is %d block(s) away from Easter Bunny HQ', $blocksAway));
+        echo (sprintf('Santa is %d block(s) away from Easter Bunny HQ', $blocksAway1) . "\n");
+        echo (sprintf('Santa is %d block(s) away from Easter Bunny HQ (First Repeated Coordinates)', $blocksAway2) . "\n");
     }
 
     /**
@@ -32,20 +40,36 @@ class BunnyHQ implements AdventOutputInterface
      *
      * @return number
      */
-    public function followDirections($directions)
+    public function followDirections($directions, $step2 = false)
     {
-        $cords  = [0, 0];
-        $facing = 'n';
+        $cords   = [0, 0];
+        $facing  = 'n';
+        $visited = ["0,0"];
 
         foreach ($directions as $direction) {
             // Split direction into needed parts.
             $turn     = $direction[0];
             $distance = (int) substr($direction, 1);
+            $previous = $cords;
 
             // Follow the turn/distance given
             $this->follow($cords, $facing, $turn, $distance);
-            // Update facing based on which direction we turned
-            //$facing = $this->getFacing($facing, $turn);
+
+            // Step 2, stop on first point visited twice
+            if ($step2) {
+                $points = $this->getAllPoints($previous, $cords);
+
+                foreach ($points as $point) {
+                    $sPoint = implode(',', $point);
+
+                    // If we are visiting this again, we stop on current cords
+                    if (in_array($sPoint, $visited)) {
+                        return $this->calculateDistanceAway($point);
+                    }
+
+                    $visited[] = $sPoint;
+                }
+            }
         }
 
         // Calculate how many blocks away we are
@@ -110,5 +134,47 @@ class BunnyHQ implements AdventOutputInterface
                 $facing    = 'n';
             }
         }
+    }
+
+    /**
+     * Generate all points between each final destination
+     *
+     * @param int[] $startingCords
+     * @param int[] $endingCords
+     *
+     * @return array
+     */
+    private function getAllPoints($startingCords, $endingCords)
+    {
+        $points = [];
+
+        // Default state is assuming starting y is less than ending y
+        $d = 0;
+        $m = 1;
+
+        // If starting y is greater than ending y
+        if ($startingCords[0] > $endingCords[0]) {
+            $m = -1;
+        }
+
+        // If starting x is less than ending x
+        if ($startingCords[1] < $endingCords[1]) {
+            $d = 1;
+
+        // If starting x is greater than ending x
+        } elseif ($startingCords[1] > $endingCords[1]) {
+            $d = 1;
+            $m = -1;
+        }
+
+        $diff = abs($startingCords[$d] - $endingCords[$d]);
+
+        // Process all points between
+        for ($i = 0; $i < $diff; $i++) {
+            $startingCords[$d] += ($m * 1);
+            $points[] = $startingCords;
+        }
+
+        return $points;
     }
 }
