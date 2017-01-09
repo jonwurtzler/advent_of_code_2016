c5a4ac05a14ac1be6b7e6e93a7d3d77815bb1860
Jon Wurtzler <jwurtzle@dhcp-16-4-70.bravo>
2017-01-09 15:47:39 -0600

    [Day 3] - Step 2 - Validate by col instead of row

--- src/Advent/ValidDesigns.php
+++ src/Advent/ValidDesigns.php
@@ -3,9 +3,12 @@
 namespace Advent;
 
 use DataLoader;
+use Exception;
 
 // Step 1: 995 is too high
 // Step 1: 993 is correct.  (Needed to be greater than, not greater than or equal to).
+
+// Step 2: 1849 is correct.
 class ValidDesigns implements AdventOutputInterface
 {
     /**
@@ -28,9 +31,21 @@ class ValidDesigns implements AdventOutputInterface
         // Load datafile
         $designs = DataLoader::loadFileAsArrayData($this->dataFile);
 
+        // Convert to int array values
+        foreach ($designs as $i => $design) {
+            $designs[$i] = array_map('intval', preg_split('/\s+/', trim($design)));
+        }
+
         // Step 1
         $validCount = $this->verifyDesigns($designs);
-        echo (sprintf('Designs: %d out of %d are valid', $validCount, count($designs)) . "\n");
+        echo (sprintf('Designs: %d out of %d are valid.', $validCount, count($designs)) . "\n");
+
+        // Reset count between steps.
+        $this->validCount = 0;
+
+        // Step 2
+        $validCount2 = $this->verifyDesigns($designs, true);
+        echo (sprintf('Designs: %d out of %d are valid. (Column Sorted)', $validCount2, count($designs)) . "\n");
     }
 
     /**
@@ -40,11 +55,13 @@ class ValidDesigns implements AdventOutputInterface
      *
      * @return int
      */
-    public function verifyDesigns($designs)
+    public function verifyDesigns($designs, $step2 = false)
     {
-        foreach ($designs as $design) {
-            $design = array_map('intval', preg_split('/\s+/', trim($design)));
+        if ($step2) {
+            $designs = $this->verifyByCol($designs);
+        }
 
+        foreach ($designs as $design) {
             if ($this->isValidTriangle($design[0], $design[1], $design[2])) {
                 $this->validCount++;
             }
@@ -77,5 +94,34 @@ class ValidDesigns implements AdventOutputInterface
         }
 
         return true;
+    }
+
+    /**
+     * Get groups of
+     *
+     * @param $designs
+     *
+     * @return array
+     * @throws Exception
+     */
+    private function verifyByCol($designs)
+    {
+        if (0 != (count($designs) %3)) {
+            throw new Exception("Please enter a number of rows evenly divided by 3");
+        }
+
+        $designsByCol = [];
+
+        for ($i = 0; $i < count($designs); $i += 3) {
+            $row1 = $designs[$i];
+            $row2 = $designs[$i + 1];
+            $row3 = $designs[$i + 2];
+
+            $designsByCol[] = [$row1[0], $row2[0], $row3[0]];
+            $designsByCol[] = [$row1[1], $row2[1], $row3[1]];
+            $designsByCol[] = [$row1[2], $row2[2], $row3[2]];
+        }
+
+        return $designsByCol;
     }
 }
--- tests/ValidDesignsTest.php
+++ tests/ValidDesignsTest.php
@@ -11,25 +11,38 @@ class ValidDesignsTest extends PHPUnit_Framework_TestCase
     public function testVerifyDesignsTC1()
     {
         $validDesigns = new ValidDesigns();
-        $this->assertEquals(0, $validDesigns->verifyDesigns(['5 10 25', '5 5 10']));
+        $this->assertEquals(0, $validDesigns->verifyDesigns([[5, 10, 25], [5, 5, 10]]));
     }
 
     public function testSmallS1S2()
     {
         $validDesigns = new ValidDesigns();
-        $this->assertEquals(0, $validDesigns->verifyDesigns(['5 10 25']));
+        $this->assertEquals(0, $validDesigns->verifyDesigns([[5, 10, 25]]));
     }
 
     public function testSmallS2S3()
     {
         $validDesigns = new ValidDesigns();
-        $this->assertEquals(0, $validDesigns->verifyDesigns(['25 10 5']));
+        $this->assertEquals(0, $validDesigns->verifyDesigns([[25, 10, 5]]));
     }
 
     public function testSmallS1S3()
     {
         $validDesigns = new ValidDesigns();
-        $this->assertEquals(0, $validDesigns->verifyDesigns(['5 25 10']));
+        $this->assertEquals(0, $validDesigns->verifyDesigns([[5, 25, 10]]));
+    }
+
+    public function testStep2Exception()
+    {
+        $validDesigns = new ValidDesigns();
+        $this->expectException(Exception::class);
+        $validDesigns->verifyDesigns([[16, 25, 10]], true);
+    }
+
+    public function testStep2()
+    {
+        $validDesigns = new ValidDesigns();
+        $this->assertEquals(3, $validDesigns->verifyDesigns([[16, 25, 10], [25, 10, 16], [10, 16, 25]], true));
     }
 
 }
